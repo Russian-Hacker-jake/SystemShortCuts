acdcf8e0db4b14dd53b6232fd23357f4113e519a
Russian-Hacker-jake <75774392+Russian-Hacker-jake@users.noreply.github.com>
2021-02-15 10:48:26 +0800

    Add files via upload

---
+++ SystemShortCuts/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace SystemShortcuts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(547, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "keep the app up for the commands to work";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(793, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "command 1 open command prompt by pressing control + t key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(731, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Command 2 Open File Explorer Press the Key Control + f";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(561, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Command 3 open pictures press control + p";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(780, 32);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Command 4 open Microsoft Team Press the key control + tab";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(616, 32);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Command 5 open Steam by pressing shift and s";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 341);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(614, 32);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Command 6 open Run by pressing control and r";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 1106);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "SystemShortCuts";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+   
+}
+
+
---
+++ SystemShortCuts/Main.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SystemShortcuts.Properties;
+using System.Runtime;
+using System.Data;
+
+namespace SystemShortcuts
+{
+
+    
+
+    public partial class main : Component
+    {
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            string userName = Environment.UserName;
+
+            if (e.Shift && e.KeyCode == Keys.F)
+            {
+                System.Diagnostics.Process.Start(@"C:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Binaries\\Win64\\FortniteLauncher.exe");
+            }
+
+
+
+            if (e.Shift && e.KeyCode == Keys.S)
+            {
+                System.Diagnostics.Process.Start(@"C:\\Program Files (x86)\\Steam\\Steam.exe");
+            }
+
+
+            if (e.Control && e.KeyCode == Keys.R)
+            {
+                System.Diagnostics.Process.Start(@"C:\\Users\\" + userName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System Tools\\Run");
+            }
+
+
+
+
+            if (e.Control && e.KeyCode == Keys.Tab)
+            {
+                System.Diagnostics.Process.Start(@"C:\\Users\\" + userName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Teams");
+
+            }
+
+
+
+
+
+
+            if (e.Control && e.KeyCode == Keys.P)
+            {
+                System.Diagnostics.Process.Start(@"C:\\Users\\" + userName + "\\AppData\\Roaming\\Microsoft\\Windows\\Libraries\\Pictures.library-ms");
+            }
+
+
+
+
+
+
+            if (e.Control && e.KeyCode == Keys.F)
+            {
+                System.Diagnostics.Process.Start(@"C:\\Users\\" + userName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System Tools\\File Explorer");
+            }
+
+
+
+
+
+
+            if (e.Control && e.KeyCode == Keys.T)
+            {
+
+                System.Diagnostics.Process.Start(@"C:\\Users\\" + userName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System Tools\\Command Prompt.lnk");
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        public main(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+
+}
+}
+
